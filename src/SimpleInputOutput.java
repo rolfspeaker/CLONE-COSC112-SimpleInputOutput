dae3dd035c34f746e8e8fba34b641f91fdb9bdb7
rolfspeaker <rolfspeaker@gmail.com>
2026-09-10 10:56:55 -0400

    Update SimpleInputOutput.java

--- src/SimpleInputOutput.java
+++ src/SimpleInputOutput.java
@@ -1,21 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-//import classes
-
-/**
- *
- * @author Instructor Base code
- */
+import java.util.Scanner;
 public class SimpleInputOutput {
-
     public static void main(String[] args) {
-        //variable declaration
+        System.out.println("Hello, world!");
+        System.out.println("My name is Ayo Odusanya.");
+        System.out.println("I am a Computer Science major at Bowie State University.");
+        System.out.println("My favorite movie is \"Inglorious Bast*rds\".");
+        System.out.println("The quote of the day is from St. Thomas Aquinas: \"There is nothing on this earth more to be prized than true friendship.\"");
         
-        //executable statements
-        
-    }
+        Scanner input_listener = new Scanner(System.in);
+        System.out.println("What is your name?");
 
+        String user_name = input_listener.nextLine();
+        System.out.println("Hello, " + user_name + "! Nice to meet you.");
+        
+        double numA, numB, numC, sum;
+        System.out.println("Enter first number:");
+
+        numA = input_listener.nextDouble();
+        System.out.println("Enter second number:");
+
+        numB = input_listener.nextDouble();
+        System.out.println("Enter third number:"); 
+
+        numC = input_listener.nextDouble(); 
+        sum = numA + numB + numC;
+        
+        System.out.println(numA + " + " + numB + " + " + numC + " = " + (int)sum);
+        input_listener.close();
+    }
 }
